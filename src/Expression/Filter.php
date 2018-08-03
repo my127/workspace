3c7b0b183d518f9a6c5124061d31b17044fb5673
David Cole <dcole@inviqa.com>
2018-08-03 09:01:17 +0100

    add expression filters to the interpreter

--- src/Expression/Filter.php
+++ src/Expression/Filter.php
@@ -1,15 +1,51 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: dcole
- * Date: 03/08/18
- * Time: 07:53
- */
 
 namespace my127\Workspace\Expression;
 
+use my127\Workspace\Interpreter\Filter as InterpreterFilter;
 
-class Filter
+class Filter implements InterpreterFilter
 {
+    const NAME = '=';
 
+    private $expression;
+
+    public function __construct(Expression $expression)
+    {
+        $this->expression = $expression;
+    }
+
+    public function getName(): string
+    {
+        return self::NAME;
+    }
+
+    public function apply(string $script): string
+    {
+        foreach ($this->findAllExpressionsInString($script) as $expression) {
+            $script = str_replace(
+                $expression,
+                $this->expression->evaluate(trim(substr($expression, 2, -1))),
+                $script
+            );
+        }
+
+        return $script;
+    }
+
+    private function findAllExpressionsInString(string $script): array
+    {
+        $expressions = [];
+
+        while (($end = $start = strpos($script, '={', $end??0)) !== false) {
+
+            while ($script[($end = strpos($script, '}', $end))-1] == '\\') {
+                ++$end;
+            }
+
+            $expressions[] = substr($script, $start, (($end + 1) - $start));
+        }
+
+        return $expressions;
+    }
 }

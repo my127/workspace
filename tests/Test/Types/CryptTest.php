9e8c5e0b86bd6a35e86bfbd8557f095f470818e5
David Cole <dcole@inviqa.com>
2018-08-11 18:13:26 +0100

    add test to ensure default key can be set via an environment variable

--- tests/Test/Types/CryptTest.php
+++ tests/Test/Types/CryptTest.php
@@ -40,4 +40,23 @@ EOD
 
         $this->assertEquals("Hello World", trim(run('hello')));
     }
+
+    /** @test */
+    function default_key_can_be_specified_as_an_environment_variable()
+    {
+        Fixture::workspace(<<<'EOD'
+
+attribute('message'): = decrypt('YTozOntpOjA7czo3OiJkZWZhdWx0IjtpOjE7czoyNDoi98rFejkefPnZG1CjzGeFyvSAMgafKv2TIjtpOjI7czoyNzoiSwcG2YiM3vV8CdZXgxDM2q+ZmRmPRNyz7OgcIjt9')
+
+command('hello'): |
+  #!bash|@
+  echo "@('message')"
+
+EOD
+        );
+
+        putenv('MY127WS_KEY=81a7fa14a8ceb8e1c8860031e2bac03f4b939de44fa1a78987a3fcff1bf57100');
+        $this->assertEquals("Hello World", trim(run('hello')));
+        putenv('MY127WS_KEY=81a7fa14a8ceb8e1c8860031e2bac03f4b939de44fa1a78987a3fcff1bf57100');
+    }
 }

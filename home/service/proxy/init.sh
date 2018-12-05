c9b0b852f74274a466e5536a65cc07b85a5e2690
Kieren Evans <kevans@inviqa.com>
2018-12-05 20:44:25 +0200

    Shellcheck fixes for home directory

--- home/service/proxy/init.sh
+++ home/service/proxy/init.sh
@@ -17,26 +17,27 @@ main()
 
 enable()
 {
-    if [ ! "$(docker ps | grep my127ws-proxy)" ]; then
-        if [ "$(docker ps -aq -f name=my127ws-proxy)" ]; then
-            { cd "${DIR}"; run docker-compose -p my127ws-proxy rm --force traefik; }
+    if ! docker ps | grep my127ws-proxy; then
+        if docker ps -aq -f name=my127ws-proxy; then
+            { cd "${DIR}" || exit 1; run docker-compose -p my127ws-proxy rm --force traefik; }
         fi
-        { cd "${DIR}"; run docker-compose -p my127ws-proxy up --build -d traefik; }
+        { cd "${DIR}" || exit 1; run docker-compose -p my127ws-proxy up --build -d traefik; }
     fi
 }
 
 disable()
 {
-    if [ "$(docker ps | grep my127ws-$1)" ]; then
-        { cd "${DIR}"; run docker-compose -p my127ws-proxy rm --stop --force traefik; }
+    if docker ps | grep my127ws-proxy; then
+        { cd "${DIR}" || exit 1; run docker-compose -p my127ws-proxy rm --stop --force traefik; }
     fi
 }
 
 bootstrap()
 {
     DIR="$(cd "$(dirname "$0")" && pwd)"
-    . "$DIR/../../lib/sidekick.sh"
+    # shellcheck source=../../lib/sidekick.sh
+    source "$DIR/../../lib/sidekick.sh"
 }
 
 bootstrap
-main $@
+main "$@"

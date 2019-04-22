b0ca40e97f23540eb15903b5d6796f853608388f
Kieren Evans <kierenevans@users.noreply.github.com>
2019-04-22 11:11:56 +0300

    Allow tests to be run on macOS
    /tmp is a symlink to /private/var/folders/RANDOMCHARS/ so the /tmp/ check fails.
    
    According to https://reactphp.org/child-process/ php uses a "sh" shellwrapper
    around all commands, of which sh's echo does not support "-n" to suppress
    the newline.

--- tests/Fixture.php
+++ tests/Fixture.php
@@ -4,43 +4,51 @@ use my127\Workspace\Utility\Filesystem;
 
 class Fixture
 {
-    private const WORKSPACE_DIR = '/tmp/my127ws.phpuint';
+    private static $WORKSPACE_DIR = '/tmp/my127ws.phpunit';
 
     private static function clean()
     {
-        if (is_dir(self::WORKSPACE_DIR)) {
-            Filesystem::rrmdir(self::WORKSPACE_DIR);
+        if (is_dir(self::$WORKSPACE_DIR)) {
+            Filesystem::rrmdir(self::$WORKSPACE_DIR);
         }
 
-        mkdir(self::WORKSPACE_DIR, 0755, true);
+        mkdir(self::$WORKSPACE_DIR, 0755, true);
+    }
+
+    private static function workspaceDir()
+    {
+        $macPrivateDir = php_uname('s') == 'Darwin' ? '/private' : '';
+        self::$WORKSPACE_DIR = $macPrivateDir . sys_get_temp_dir() . '/my127ws.phpunit';
     }
 
     public static function sampleData($name): string
     {
         self::clean();
+        self::workspaceDir();
 
         if (!is_dir($src = __DIR__.'/samples/'.$name)) {
             throw new Exception("Fixture '{$name}' not found.");
         }
 
-        Filesystem::rcopy($src, self::WORKSPACE_DIR);
-        chdir(self::WORKSPACE_DIR);
-        return self::WORKSPACE_DIR;
+        Filesystem::rcopy($src, self::$WORKSPACE_DIR);
+        chdir(self::$WORKSPACE_DIR);
+        return self::$WORKSPACE_DIR;
     }
 
     public static function workspace($declarations)
     {
         self::clean();
-        file_put_contents(self::WORKSPACE_DIR.'/workspace.yml', $declarations);
-        chdir(self::WORKSPACE_DIR);
-        return self::WORKSPACE_DIR;
+        self::workspaceDir();
+        file_put_contents(self::$WORKSPACE_DIR.'/workspace.yml', $declarations);
+        chdir(self::$WORKSPACE_DIR);
+        return self::$WORKSPACE_DIR;
     }
 
     public static function workspaceWithSampleData($declarations, $sample)
     {
         self::sampleData($sample);
-        file_put_contents(self::WORKSPACE_DIR.'/workspace.yml', $declarations);
-        chdir(self::WORKSPACE_DIR);
-        return self::WORKSPACE_DIR;
+        file_put_contents(self::$WORKSPACE_DIR.'/workspace.yml', $declarations);
+        chdir(self::$WORKSPACE_DIR);
+        return self::$WORKSPACE_DIR;
     }
 }

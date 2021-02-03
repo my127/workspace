6e055765a1cf6da5f6018c35797b2419d03f7e8a
Kieren Evans <kevans@inviqa.com>
2021-02-03 18:43:05 +0200

    Add second path for config that isn't owned by root

--- home/service/tracing/init.sh
+++ home/service/tracing/init.sh
@@ -29,27 +29,21 @@ enable()
     run docker-compose -p my127ws-tracing pull
     run docker-compose -p my127ws-tracing up -d
     run cp -pR "$DIR/traefik/root/config/tracing.toml" "$DIR/../proxy/traefik/root/config/"
-    passthru ws global service proxy restart
 )
 
 disable()
 (
-    local SKIP_RESTART="${1:-}"
-
     cd "$DIR"
 
     if [ -f "$DIR/../proxy/traefik/root/config/tracing.toml" ]; then
       run rm -f "$DIR/../proxy/traefik/root/config/tracing.toml"
     fi
     run docker-compose -p my127ws-tracing down -v --rmi local
-    if [ "$SKIP_RESTART" != "skip" ]; then
-      passthru ws global service proxy restart
-    fi
 )
 
 restart()
 {
-    disable "skip"
+    disable
     enable
 }
 

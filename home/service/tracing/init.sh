a88dd02b56e961335d3b8d9424f6920ffeb3033d
Kieren Evans <kierenevans@users.noreply.github.com>
2021-03-24 18:40:02 +0200

    Add Jaeger integration to Traefik (#69)

--- home/service/tracing/init.sh
+++ home/service/tracing/init.sh
@@ -6,13 +6,13 @@ DIR=""
 
 main()
 {
-    if [ "$1" = "enable" ]; then
-        enable
+    if [ "$1" = "enable" ] || [ "$1" = "start" ]; then
+        start
         exit
     fi
 
-    if [ "$1" = "disable" ]; then
-        disable
+    if [ "$1" = "disable" ] || [ "$1" = "stop" ]; then
+        stop
         exit
     fi
 
@@ -22,24 +22,43 @@ main()
     fi
 }
 
-enable()
+start()
 (
     cd "$DIR"
 
+    run docker-compose -p my127ws-tracing pull
     run docker-compose -p my127ws-tracing up -d
+
+    local TRAEFIK_CONFIG="${DIR}/../proxy/traefik/root/traefik.toml"
+    if grep -q '\[inactive.tracing\]' "${TRAEFIK_CONFIG}"; then
+      cp "${TRAEFIK_CONFIG}" "${TRAEFIK_CONFIG}.before-tracing-active"
+      sed 's/\[inactive.tracing\]/\[tracing\]/' "${TRAEFIK_CONFIG}.before-tracing-active" > "${TRAEFIK_CONFIG}"
+      rm "${TRAEFIK_CONFIG}.before-tracing-active"
+      passthru ws global service proxy restart
+    fi
 )
 
-disable()
+stop()
 (
+    local DO_PROXY_RESTART="${1:-yes}"
     cd "$DIR"
 
+    local TRAEFIK_CONFIG="${DIR}/../proxy/traefik/root/traefik.toml"
+    if [ "$DO_PROXY_RESTART" = "yes" ] && grep -q '\[tracing\]' "${TRAEFIK_CONFIG}"; then
+      cp "${TRAEFIK_CONFIG}" "${TRAEFIK_CONFIG}.before-tracing-inactive"
+      sed 's/\[tracing\]/\[inactive.tracing\]/' "${TRAEFIK_CONFIG}.before-tracing-inactive" > "${TRAEFIK_CONFIG}"
+      rm "${TRAEFIK_CONFIG}.before-tracing-inactive"
+      if [ "$DO_PROXY_RESTART" = "yes" ]; then
+        passthru ws global service proxy restart
+      fi
+    fi
     run docker-compose -p my127ws-tracing down -v --rmi local
 )
 
 restart()
 {
-    disable
-    enable
+    stop "no"
+    start
 }
 
 bootstrap()

5279bf5866623eb09cf081164680f3e141233000
Kieren Evans <kevans@inviqa.com>
2021-06-09 21:05:27 +0300

    Optional argument tests

--- tests/Test/Types/CommandTest.php
+++ tests/Test/Types/CommandTest.php
@@ -268,6 +268,22 @@ EOD
     }
 
     /** @test */
+    public function optional_arguments_can_be_specified()
+    {
+        $this->createWorkspaceYml(<<<'EOD'
+attribute('default_value'): 'default value'
+
+command('hello [<name>]'):
+  exec: |
+    #!bash|=
+    echo -n "hello ={input.argument('name') ?: 'default value'}"
+EOD
+        );
+
+        $this->assertEquals('hello world', $this->workspaceCommand('hello world')->getOutput());
+    }
+
+    /** @test */
     public function optional_options_can_be_specified()
     {
         $this->createWorkspaceYml(<<<'EOD'
@@ -283,6 +299,22 @@ EOD
         );
 
         $this->assertEquals('hello world', $this->workspaceCommand('hello --name=world')->getOutput());
+    }
+
+    /** @test */
+    public function optional_arguments_can_be_left_empty()
+    {
+        $this->createWorkspaceYml(<<<'EOD'
+attribute('default_value'): 'default value'
+
+command('hello [<name>]'):
+  exec: |
+    #!bash|=
+    echo -n "hello ={input.argument('name') ?: 'default value'}"
+EOD
+        );
+
+        $this->assertEquals('hello default value', $this->workspaceCommand('hello')->getOutput());
     }
 
     /** @test */

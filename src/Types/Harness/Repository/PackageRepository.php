8c7e27420f7fa2c0c4696d4c360a5bfad88ea202
Gavin Foster <gavin.foster@inviqa.com>
2022-12-14 10:46:56 +0000

    Fix PHPStan errors

--- src/Types/Harness/Repository/PackageRepository.php
+++ src/Types/Harness/Repository/PackageRepository.php
@@ -158,9 +158,9 @@ class PackageRepository implements Repository
     {
         if (preg_match(self::HARNESS_VERSION_PATTERN, $version, $match)) {
             return [
-                isset($match['major']) ? (int) $match['major'] : 'x',
-                isset($match['minor']) ? (int) $match['minor'] : 'x',
-                isset($match['patch']) ? (int) $match['patch'] : 'x',
+                isset($match['major']) && is_numeric($match['major']) ? (int) $match['major'] : 'x',
+                isset($match['minor']) && is_numeric($match['minor']) ? (int) $match['minor'] : 'x',
+                isset($match['patch']) && is_numeric($match['patch']) ? (int) $match['patch'] : 'x',
             ];
         }
 

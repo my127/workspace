ff3c1a2bddc0ec7d5dced9b917f99e6b7a6ee7c4
Gavin Foster <gavin.foster@inviqa.com>
2022-12-14 10:01:46 +0000

    Commit Andy suggestion

--- tools/builder/root/entrypoint.sh
+++ tools/builder/root/entrypoint.sh
@@ -27,6 +27,8 @@ resolve_volume_mount_strategy()
             STRATEGY="host-osx-normal"
         elif (mount | grep "/app type fuse.grpcfuse") > /dev/null 2>&1; then
             STRATEGY="host-osx-normal"
+        elif (mount | grep "/app type virtiofs") > /dev/null 2>&1; then
+            STRATEGY="host-osx-normal"
         else
             exit 1
         fi

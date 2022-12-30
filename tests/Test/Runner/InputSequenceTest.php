3d91b0d2c1809499178cd4d1f88460c21e227abf
Daniel Leech <dan.t.leech@gmail.com>
2022-12-30 15:18:43 +0000

    Add test for "get argument string"

--- tests/Test/Runner/InputSequenceTest.php
+++ tests/Test/Runner/InputSequenceTest.php
@@ -9,5 +9,7 @@ class InputSequenceTest extends TestCase
 {
     public function testGetArgumentString(): void
     {
+        self::assertEquals('', (new InputSequence([], []))->toArgumentString());
+        self::assertEquals('foo bar', (new InputSequence([], ['bar' ,'foo', 'ws']))->toArgumentString());
     }
 }

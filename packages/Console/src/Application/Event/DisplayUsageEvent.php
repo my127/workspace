7c877088d7fb11c974a692465902b5b755b4360f
Gavin Foster <47179450+g-foster2@users.noreply.github.com>
2023-02-15 09:51:17 +0000

    Fixes #144 - Exit code and output stream changes (#145)
    * Fixes #144 - Exit code and output stream changes
    
    * PHP CS Fixer
    
    * Fixes #144 - Exit code and output stream changes
    
    * Add tests

--- packages/Console/src/Application/Event/DisplayUsageEvent.php
+++ packages/Console/src/Application/Event/DisplayUsageEvent.php
@@ -7,11 +7,11 @@ use my127\Console\Usage\Parser\InputSequence;
 use my127\Console\Usage\Parser\InputSequenceFactory;
 use Symfony\Contracts\EventDispatcher\Event;
 
-class InvalidUsageEvent extends Event
+class DisplayUsageEvent extends Event
 {
     private InputSequence $input;
 
-    public function __construct($args, OptionDefinitionCollection $options)
+    public function __construct($args, private OptionDefinitionCollection $options, private bool $validCommand = true)
     {
         $this->input = (new InputSequenceFactory())->createFrom($args, $options, true);
     }
@@ -19,5 +19,10 @@ class InvalidUsageEvent extends Event
     public function getInputSequence(): InputSequence
     {
         return clone $this->input;
+    }
+
+    public function validCommand(): bool
+    {
+        return $this->validCommand;
     }
 }

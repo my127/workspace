d854f7d35cc5c406db83580f918062e8f1abf4d6
andytson-inviqa <andytson-inviqa@users.noreply.github.com>
2023-02-20 14:06:55 +0000

    Avoid using addslashes in bash executor and pass in safely (#149)
    * proc_open supports an env parameter, but needs explicit adding of previous env as well if used
    * bash scripts can receive positional arguments, so use them and map them back to their bash variable name
      * arguments are not exposed as an option anywhere yet, other than through expressions in env
    * proc_open supports auto-escaping of array commands
    * proc_open supports cwd setting as well, but I'm avoiding it due to not supporting relative path unlike the cd in the script

--- tests/Test/Interpreters/BashTest.php
+++ tests/Test/Interpreters/BashTest.php
@@ -21,4 +21,95 @@ EOD
             $this->workspaceCommand('speak')->getOutput()
         );
     }
+
+    public function bashCanReceiveParentEnvironmentVariables()
+    {
+        putenv('MESSAGE=Hello World');
+        $this->createWorkspaceYml(<<<'EOD'
+command('speak'):
+  exec: |
+    #!bash
+    echo -n "$MESSAGE"
+EOD
+        );
+
+        $this->assertEquals(
+            'Hello World',
+            $this->workspaceCommand('speak')->getOutput()
+        );
+    }
+
+    public function bashCanReceiveCustomEnvironmentVariables()
+    {
+        $this->createWorkspaceYml(<<<'EOD'
+command('speak'):
+  env:
+    MESSAGE: Hello World
+  exec: |
+    #!bash
+    echo -n "$MESSAGE"
+EOD
+        );
+
+        $this->assertEquals(
+            'Hello World',
+            $this->workspaceCommand('speak')->getOutput()
+        );
+    }
+
+    public function bashCanReceiveParentEnvironmentVariablesWhileCustomUsed()
+    {
+        putenv('REAL_MESSAGE=Hello World');
+        $this->createWorkspaceYml(<<<'EOD'
+command('speak'):
+  env:
+    MESSAGE: I'm sorry Dave, I'm afraid I can't do that
+  exec: |
+    #!bash
+    echo -n "$REAL_MESSAGE"
+EOD
+        );
+
+        $this->assertEquals(
+            'Hello World',
+            $this->workspaceCommand('speak')->getOutput()
+        );
+    }
+
+    public function bashCanReceiveOverrideParentEnvironmentVariables()
+    {
+        putenv('MESSAGE=I should be overridden');
+        $this->createWorkspaceYml(<<<'EOD'
+command('speak'):
+  env:
+    MESSAGE: Hello World
+  exec: |
+    #!bash
+    echo -n "$MESSAGE"
+EOD
+        );
+
+        $this->assertEquals(
+            'Hello World',
+            $this->workspaceCommand('speak')->getOutput()
+        );
+    }
+
+    public function bashCanReceiveQuotesInEnvironmentVariables()
+    {
+        $this->createWorkspaceYml(<<<'EOD'
+command('speak'):
+  env:
+    MESSAGE: someone said 'Hello World'
+  exec: |
+    #!bash
+    echo -n "$MESSAGE"
+EOD
+        );
+
+        $this->assertEquals(
+            'someone said \'Hello World\'',
+            $this->workspaceCommand('speak')->getOutput()
+        );
+    }
 }

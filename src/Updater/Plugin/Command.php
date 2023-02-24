5e82a0d6220faf96227de988bd220859a3556b9d
Andy Thompson <athompson@inviqa.com>
2023-02-24 12:59:30 +0000

    Update self-update to support an optional version constraint
    It will limit the release to the highest matching candidate based on a min stability default to stable

--- src/Updater/Plugin/Command.php
+++ src/Updater/Plugin/Command.php
@@ -4,6 +4,7 @@ namespace my127\Workspace\Updater\Plugin;
 
 use my127\Console\Application\Application;
 use my127\Console\Application\Plugin\Plugin;
+use my127\Console\Usage\Input;
 use my127\Workspace\Application as BaseApplication;
 use my127\Workspace\Updater\Exception\NoUpdateAvailableException;
 use my127\Workspace\Updater\Exception\NoVersionDeterminedException;
@@ -23,30 +24,33 @@ class Command implements Plugin
     {
         $application->section('self-update')
             ->description('Updates the current version of workspace.')
-            ->action($this->action());
+            ->usage('self-update [<version-constraint>]')
+            ->action(fn (Input $input) => $this->action($input));
     }
 
-    private function action()
+    private function action(Input $input)
     {
-        return function () {
-            $pharPath = \Phar::running(false);
-            if (empty($pharPath)) {
-                echo 'This command can only be executed from within the ws utility.' . PHP_EOL;
-                exit(1);
-            }
+        $pharPath = \Phar::running(false);
+        if (empty($pharPath)) {
+            echo 'This command can only be executed from within the ws utility.' . PHP_EOL;
+            exit(1);
+        }
 
-            try {
+        try {
+            if ($input->getArgument('version-constraint')) {
+                $this->updater->update(BaseApplication::getVersion(), $input->getArgument('version-constraint'), $pharPath);
+            } else {
                 $this->updater->updateLatest(BaseApplication::getVersion(), $pharPath);
-            } catch (NoUpdateAvailableException $e) {
-                echo sprintf('You are already running the latest version of workspace: %s', $e->getCurrentVersion()) . PHP_EOL;
-                exit(1);
-            } catch (NoVersionDeterminedException $e) {
-                echo 'Unable to determine your current workspace version. You are likely not using a tagged released.' . PHP_EOL;
-                exit(1);
-            } catch (\RuntimeException $e) {
-                echo sprintf('%s. Aborting self-update', $e->getMessage()) . PHP_EOL;
-                exit(1);
             }
-        };
+        } catch (NoUpdateAvailableException $e) {
+            echo sprintf('You are already running the latest version of workspace: %s', $e->getCurrentVersion()) . PHP_EOL;
+            exit(1);
+        } catch (NoVersionDeterminedException $e) {
+            echo 'Unable to determine your current workspace version. You are likely not using a tagged released.' . PHP_EOL;
+            exit(1);
+        } catch (\RuntimeException $e) {
+            echo sprintf('%s. Aborting self-update', $e->getMessage()) . PHP_EOL;
+            exit(1);
+        }
     }
 }
--- tests/Test/Updater/UpdaterTest.php
+++ tests/Test/Updater/UpdaterTest.php
@@ -24,6 +24,16 @@ class UpdaterTest extends TestCase
         }
     }
 
+    protected function tearDown(): void
+    {
+        if (file_exists(__DIR__ . '/fixtures/generated/fake.phar')) {
+            \Phar::unlinkArchive(__DIR__ . '/fixtures/generated/fake.phar');
+        }
+        if (file_exists(__DIR__ . '/fixtures/generated/fake.phar.tar')) {
+            \Phar::unlinkArchive(__DIR__ . '/fixtures/generated/fake.phar.tar');
+        }
+    }
+
     public static function setUpBeforeClass(): void
     {
         if (!is_dir(__DIR__ . '/fixtures/generated')) {
@@ -93,6 +103,79 @@ class UpdaterTest extends TestCase
         $updater->updateLatest('0.9.0', $temp);
 
         $this->assertFileExists($temp);
+    }
+
+    /** @test */
+    public function downloadsLatestCandidateOfConstraintToDesiredTargetPath()
+    {
+        $this->prepareFakePhar();
+        $this->prepareReleasesFixture('valid.json', __DIR__ . '/fixtures/generated/fake.phar', '1.0.0', '0.9.1');
+
+        $temp = sys_get_temp_dir() . '/test-ws-download';
+        $updater = new Updater(__DIR__ . '/fixtures/generated/valid.json', new NullOutput());
+        $updater->update('0.9.0', '~0.9.1', $temp);
+
+        $this->assertFileExists($temp);
+    }
+
+    /** @test */
+    public function downloadsLatestStableCandidateOfConstraintToDesiredTargetPath()
+    {
+        $this->prepareFakePhar();
+        $this->prepareReleasesFixture('valid.json', __DIR__ . '/fixtures/generated/fake.phar', '1.0.0-alpha1', '0.9.1');
+
+        $temp = sys_get_temp_dir() . '/test-ws-download';
+        $updater = new Updater(__DIR__ . '/fixtures/generated/valid.json', new NullOutput());
+        $updater->update('0.9.0', '~0.9.1', $temp);
+
+        $this->assertFileExists($temp);
+    }
+
+    /** @test */
+    public function exceptionThrownWhenNoStableCandidate()
+    {
+        $this->prepareFakePhar();
+        $this->prepareReleasesFixture('valid.json', __DIR__ . '/fixtures/generated/fake.phar', '1.0.0-alpha1', '0.9.1-alpha1');
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionCode(Updater::CODE_ERR_FETCHING_RELEASES);
+
+        $temp = sys_get_temp_dir() . '/test-ws-download';
+        $updater = new Updater(__DIR__ . '/fixtures/generated/valid.json', new NullOutput());
+        $updater->update('0.9.0', '~0.9.1', $temp);
+    }
+
+    /** @test */
+    public function exceptionThrownWhenNoRCCandidate()
+    {
+        $this->prepareFakePhar();
+        $this->prepareReleasesFixture('valid.json', __DIR__ . '/fixtures/generated/fake.phar', '1.0.0-alpha1', '0.9.1-alpha1');
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionCode(Updater::CODE_ERR_FETCHING_RELEASES);
+
+        $temp = sys_get_temp_dir() . '/test-ws-download';
+        $updater = new Updater(__DIR__ . '/fixtures/generated/valid.json', new NullOutput());
+        $updater->update('0.9.0', '~0.9.1@RC', $temp);
+    }
+
+    /** @test */
+    public function downloadsLatestAlphaCandidateOfConstraintToDesiredTargetPath()
+    {
+        $this->prepareFakePhar();
+        $this->prepareReleasesFixture('valid.json', __DIR__ . '/fixtures/generated/fake.phar', '1.0.0-alpha1', '0.9.1-alpha1');
+
+        $temp = sys_get_temp_dir() . '/test-ws-download';
+        $updater = new Updater(__DIR__ . '/fixtures/generated/valid.json', new NullOutput());
+        $updater->update('0.9.0', '~0.9.0@alpha', $temp);
+
+        $this->assertFileExists($temp);
+    }
+
+    private function prepareReleasesFixture(string $name, string $releasePath, string $version1, string $version2): void
+    {
+        $contents = file_get_contents(__DIR__ . '/fixtures/tpl/releases.json');
+        $contents = str_replace(['%%browserDownloadUrl%%', '%%versionTag1%%', '%%versionTag2%%'], [$releasePath, $version1, $version2], $contents);
+
+        file_put_contents(__DIR__ . '/fixtures/generated/' . $name, $contents);
     }
 
     private function prepareLatestFixture(string $name, string $releasePath, string $version): void

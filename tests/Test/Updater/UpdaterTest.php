ba0808f0fa228640a8a6911d964527ed8ca013e1
andytson-inviqa <andytson-inviqa@users.noreply.github.com>
2023-09-22 14:41:42 +0100

    Stop self-update picking releases that are not the latest (#153)
    We will be using pre-release tags for testing, which would be listed
    later than the latest tag

--- tests/Test/Updater/UpdaterTest.php
+++ tests/Test/Updater/UpdaterTest.php
@@ -11,7 +11,17 @@ class UpdaterTest extends TestCase
 {
     public static function tearDownAfterClass(): void
     {
-        array_map('unlink', glob(__DIR__ . '/fixtures/generated/*.*'));
+        $files = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator(__DIR__ . '/fixtures/generated', \RecursiveDirectoryIterator::SKIP_DOTS),
+            \RecursiveIteratorIterator::CHILD_FIRST
+        );
+        foreach ($files as $file) {
+            if ($file->isDir()) {
+                rmdir($file);
+            } else {
+                unlink($file);
+            }
+        }
     }
 
     public static function setUpBeforeClass(): void
@@ -28,76 +38,70 @@ class UpdaterTest extends TestCase
         $this->expectExceptionCode(Updater::CODE_ERR_FETCHING_RELEASES);
 
         $updater = new Updater(__DIR__ . '/fixtures/foo.json', new NullOutput());
-        $updater->update('1.0.0', '');
-    }
-
-    /** @test */
-    public function exceptionThrownWhenThereAreNoReleases()
-    {
-        $this->expectException(\RuntimeException::class);
-        $this->expectExceptionCode(Updater::CODE_NO_RELEASES);
-
-        $updater = new Updater(__DIR__ . '/fixtures/empty-releases.json');
-        $updater->update('1.0.0', '');
+        $updater->updateLatest('1.0.0', '');
     }
 
     /** @test */
     public function exceptionThrownWhenAlreadyOnLatest()
     {
-        $this->prepareReleasesFixture('latest.json', '', '1.0.0');
+        $this->prepareLatestFixture('valid/latest', '', '1.0.0');
         $this->expectException(NoUpdateAvailableException::class);
 
-        $updater = new Updater(__DIR__ . '/fixtures/generated/latest.json', new NullOutput());
-        $updater->update('1.0.0', '');
+        $updater = new Updater(__DIR__ . '/fixtures/generated/valid', new NullOutput());
+        $updater->updateLatest('1.0.0', '');
     }
 
     /** @test */
     public function exceptionThrownWhenCurrentVersionIsEmpty()
     {
-        $this->prepareReleasesFixture('latest.json', '', '1.0.0');
+        $this->prepareLatestFixture('valid/latest', '', '1.0.0');
         $this->expectException(NoVersionDeterminedException::class);
 
-        $updater = new Updater(__DIR__ . '/fixtures/generated/latest.json', new NullOutput());
-        $updater->update('', '');
+        $updater = new Updater(__DIR__ . '/fixtures/generated/valid', new NullOutput());
+        $updater->updateLatest('', '');
     }
 
     /** @test */
     public function exceptionThrownWhenOnMoreRecentVersion()
     {
-        $this->prepareReleasesFixture('older.json', '', '1.0.0');
+        $this->prepareLatestFixture('older/latest', '', '1.0.0');
         $this->expectException(NoUpdateAvailableException::class);
 
-        $updater = new Updater(__DIR__ . '/fixtures/generated/older.json', new NullOutput());
-        $updater->update('1.1.0', '');
+        $updater = new Updater(__DIR__ . '/fixtures/generated/older', new NullOutput());
+        $updater->updateLatest('1.1.0', '');
     }
 
     /** @test */
     public function exceptionThrownWhenNextReleaseCannotBeDownloaded()
     {
-        $this->prepareReleasesFixture('invalid-release.json', __DIR__ . '/foo.baz.bar', '1.0.0');
+        $this->prepareLatestFixture('invalid-release/latest', __DIR__ . '/foo.baz.bar', '1.0.0');
         $this->expectException(\RuntimeException::class);
         $this->expectExceptionCode(Updater::CODE_ERR_FETCHING_NEXT_RELEASE);
 
-        $updater = new Updater(__DIR__ . '/fixtures/generated/invalid-release.json', new NullOutput());
-        $updater->update('0.9.0', '');
+        $updater = new Updater(__DIR__ . '/fixtures/generated/invalid-release', new NullOutput());
+        $updater->updateLatest('0.9.0', '');
     }
 
     /** @test */
     public function downloadsLatestReleaseToDesiredTargetPath()
     {
         $this->prepareFakePhar();
-        $this->prepareReleasesFixture('valid.json', __DIR__ . '/fixtures/generated/fake.phar', '1.0.0');
+        $this->prepareLatestFixture('valid/latest', __DIR__ . '/fixtures/generated/fake.phar', '1.0.0');
 
         $temp = sys_get_temp_dir() . '/test-ws-download';
-        $updater = new Updater(__DIR__ . '/fixtures/generated/valid.json', new NullOutput());
-        $updater->update('0.9.0', $temp);
+        $updater = new Updater(__DIR__ . '/fixtures/generated/valid', new NullOutput());
+        $updater->updateLatest('0.9.0', $temp);
 
         $this->assertFileExists($temp);
     }
 
-    private function prepareReleasesFixture(string $name, string $releasePath, string $version): void
+    private function prepareLatestFixture(string $name, string $releasePath, string $version): void
     {
-        $contents = file_get_contents(__DIR__ . '/fixtures/tpl/releases.json');
+        $dir = dirname(__DIR__ . '/fixtures/generated/' . $name);
+        if (!file_exists($dir)) {
+            mkdir($dir);
+        }
+        $contents = file_get_contents(__DIR__ . '/fixtures/tpl/latest.json');
         $contents = str_replace(['%%browserDownloadUrl%%', '%%versionTag%%'], [$releasePath, $version], $contents);
 
         file_put_contents(__DIR__ . '/fixtures/generated/' . $name, $contents);
